cbfdbfd751f1b7898d25d3766fc88201818def87
Kyrylo Cherdakliiev <yungkeryaa@gmail.com>
2026-01-12 11:47:43 +0100

    implemented task and all tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,6 +3,7 @@ from typing import Any
 import pytest
 from app.restore_names import restore_names
 
+
 @pytest.mark.parametrize(
     "users,expected_users",
     [
@@ -12,24 +13,24 @@ from app.restore_names import restore_names
                     "first_name": None,
                     "last_name": "Holy",
                     "full_name": "Jack Holy",
-                  },
-                  {
+                },
+                {
                     "last_name": "Adams",
                     "full_name": "Mike Adams",
-                  },
+                },
             ],
-                [
-                  {
+            [
+                {
                     "first_name": "Jack",
                     "last_name": "Holy",
                     "full_name": "Jack Holy",
-                  },
-                  {
+                },
+                {
                     "first_name": "Mike",
                     "last_name": "Adams",
                     "full_name": "Mike Adams",
-                  },
-                ],
+                },
+            ],
             id="boundary tests"
         )
     ]
@@ -38,7 +39,7 @@ def test_works_correctly(
         users: list[dict],
         expected_users: list[dict]) -> None:
     restore_names(users)
-    assert  users == expected_users
+    assert users == expected_users
 
 
 @pytest.mark.parametrize(
